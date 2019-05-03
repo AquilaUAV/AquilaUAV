7e191c576840cf3624e4549fe6b2ce0b0d08a48d
d3dx13 <zhidkov2846@gmail.com>
2019-05-03 09:52:51 +0300

    add cpp flow version

--- aquila_cpp/main.cpp
+++ aquila_cpp/main.cpp
@@ -126,7 +126,7 @@ int main( int argc, char** argv )
         needToInit = false;
         imshow("LK Demo", image);
 
-        char c = (char)waitKey(10);
+        char c = (char)waitKey(1);
         if( c == 27 )
             break;
         switch( c )
